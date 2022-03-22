8078bc3fb94105ca8fada803d394d63099d48c37
Guilherme Viana <ggviana@hotmail.com.br>
2022-03-22 16:20:05 -0300

    Removed TS prettier plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
   plugins: ["@typescript-eslint"],
   extends: [
     "standard",
-    "plugin:prettier/recommended",
     "plugin:node/recommended",
   ],
   parser: "@typescript-eslint/parser",
