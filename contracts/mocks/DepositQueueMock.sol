628489f2af46dd5567e6e7e5a640d5d8bea521bb
Guilherme Viana <ggviana@hotmail.com.br>
2022-04-21 11:41:19 -0300

    Added DepositQueue logic

---
+++ contracts/mocks/DepositQueueMock.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.6;
+
+import "../libs/DepositQueueLib.sol";
+
+
+contract DepositQueueMock {
+    using DepositQueueLib for DepositQueueLib.DepositQueue;
+
+    DepositQueueLib.DepositQueue private depositQueue;
+
+    function push(uint amount) external {
+        depositQueue.push(DepositQueueLib.DepositEntry(msg.sender, amount));
+    }
+
+    function remove(uint startIndex, uint endIndex) external {
+        depositQueue.remove(startIndex, endIndex);
+    }
+
+    function get(uint index) external view returns(DepositQueueLib.DepositEntry memory) {
+        return depositQueue.get(index);
+    }
+
+    function size() external view returns(uint) {
+        return depositQueue.size();
+    }
+}
