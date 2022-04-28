7724b5a41a8f641e8e692d8d70b3eafa0b8b1a65
Robsonsjre <robsonsjre@gmail.com>
2022-04-27 21:36:18 -0300

    hotfix: fix deposit check

--- contracts/vaults/PrincipalProtectedETHBull.sol
+++ contracts/vaults/PrincipalProtectedETHBull.sol
@@ -25,8 +25,7 @@ contract PrincipalProtectedETHBull is BaseVault {
         address _strategist,
         address _investor,
         address _pool
-    ) BaseVault(_underlying, _strategist)
-    {
+    ) BaseVault(_underlying, _strategist) {
         investor = _investor;
         pool = YieldSourceMock(_pool);
     }
@@ -34,30 +33,32 @@ contract PrincipalProtectedETHBull is BaseVault {
     /**
      * @dev See {IVault-name}.
      */
-    function name() external override pure returns(string memory) {
+    function name() external pure override returns (string memory) {
         return "Principal Protected ETH Bull";
     }
 
-    function _afterRoundStart(uint assets) internal override {
-        pool.deposit(assets, address(this));
+    function _afterRoundStart(uint256 assets) internal override {
+        if (assets > 0) {
+            pool.deposit(assets, address(this));
+        }
         lastRoundBalance = totalAssets();
     }
 
     function _afterRoundEnd() internal override {
-        uint underlyingBefore = asset.balanceOf(address(this));
+        uint256 underlyingBefore = asset.balanceOf(address(this));
         // Marks the amount interest gained in the round
-        uint interest = totalAssets() - lastRoundBalance;
+        uint256 interest = totalAssets() - lastRoundBalance;
         // Pulls the yields from investor
-        uint investmentYield = asset.balanceOf(investor);
-        if(investmentYield > 0) {
+        uint256 investmentYield = asset.balanceOf(investor);
+        if (investmentYield > 0) {
             asset.safeTransferFrom(investor, address(this), investmentYield);
         }
 
-        uint toPosition = asset.balanceOf(address(this)) - underlyingBefore;
+        uint256 toPosition = asset.balanceOf(address(this)) - underlyingBefore;
         pool.deposit(toPosition, address(this));
 
         // Send round investment to investor
-        uint investment = interest * investorRatio / DENOMINATOR;
+        uint256 investment = (interest * investorRatio) / DENOMINATOR;
         pool.withdraw(investment);
         asset.safeTransfer(investor, investment);
     }
@@ -65,7 +66,7 @@ contract PrincipalProtectedETHBull is BaseVault {
     /**
      * @dev See {BaseVault-totalAssets}.
      */
-    function totalAssets() public override view returns(uint) {
+    function totalAssets() public view override returns (uint256) {
         return pool.previewRedeem(pool.balanceOf(address(this)));
     }
 
