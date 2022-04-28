0494712d65d3ee24aedbd4baec8721c314d01387
Robsonsjre <robsonsjre@gmail.com>
2022-04-28 10:51:24 -0300

    feature: added verify step to the scripts

---
+++ scripts/verify.ts
@@ -0,0 +1,27 @@
+import retry from './retry'
+
+export default async function verify (hre: any, address: string, constructorArguments: any = [], libraries: Object) {
+  console.log('--Starting Verify Process--')
+  const verifyData = {
+    address,
+    constructorArguments,
+    libraries
+  }
+
+  if (libraries) {
+    verifyData.libraries = libraries
+  }
+
+  await retry(() => hre.run('verify:verify', verifyData), 3, (error: any) => {
+    const message = error.message.match(/Reason: (.*)/m)
+    if (message === null || message.length < 2) {
+      return true
+    }
+    const reason = message[1]
+    const isVerified = reason === 'Already Verified'
+    if (isVerified) {
+      console.log(error.message)
+    }
+    return !isVerified
+  })
+}
