05ae4ab96f147b96e6939b4b8873598f6e0fab70
Guilherme Viana <ggviana@hotmail.com.br>
2022-05-26 01:03:19 -0300

    Fixed verify wiith args

--- scripts/deployTestnet.ts
+++ scripts/deployTestnet.ts
@@ -12,11 +12,14 @@ async function main (): Promise<void> {
   const [deployer] = await ethers.getSigners()
   const deployerAddress = await deployer.getAddress()
 
+  const assetName = 'Liquid staked Ether 2.0'
+  const assetSymbol = 'stETH'
+
   const Asset = await ethers.getContractFactory('Asset')
-  const asset = await Asset.deploy('Liquid staked Ether 2.0', 'stETH')
+  const asset = await Asset.deploy(assetName, assetSymbol)
   await asset.deployTransaction.wait(WAIT_CONFIRMATIONS)
   console.log(`\n${await asset.symbol()} deployed at: ${asset.address}\n`)
-  await verifyContract(hre, asset.address)
+  await verifyContract(hre, asset.address, [assetName, assetSymbol])
 
   const YieldSource = await ethers.getContractFactory('YieldSourceMock')
   const yieldSource = await YieldSource.deploy(asset.address)
@@ -49,7 +52,7 @@ async function main (): Promise<void> {
   await verifyContract(hre, vault.address, vaultConstructorArguments)
 
   /* eslint-disable-next-line @typescript-eslint/no-floating-promises */
-  ;(await investor.approveVaultToPull(vault.address)).wait(WAIT_CONFIRMATIONS)
+  await (await investor.approveVaultToPull(vault.address)).wait(WAIT_CONFIRMATIONS)
 
   console.table({
     Asset: asset.address,
