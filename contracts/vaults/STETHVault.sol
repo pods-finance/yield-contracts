8f2185920ba4eb55ba760cd0d0b772223f3e3925
Guilherme Viana <ggviana@hotmail.com.br>
2022-06-06 01:49:15 -0300

    Simulating Lido integration

--- contracts/vaults/STETHVault.sol
+++ contracts/vaults/STETHVault.sol
@@ -7,15 +7,22 @@ import "./BaseVault.sol";
  * @title A Vault that use variable weekly yields to buy calls
  * @author Pods Finance
  */
-contract STETHBullVault is BaseVault {
+contract STETHVault is BaseVault {
     using TransferUtils for IERC20Metadata;
     using FixedPointMath for uint256;
+    using FixedPointMath for FixedPointMath.Fractional;
 
     uint256 public constant DENOMINATOR = 10000;
 
+    uint8 public immutable sharePriceDecimals;
     uint256 public lastRoundAssets;
+    FixedPointMath.Fractional public lastSharePrice;
+
     uint256 public investorRatio = 5000;
     address public investor;
+
+    event RoundData(uint256 indexed roundId, uint256 roundAccruedInterest, uint256 investmentYield, uint256 idleAssets);
+    event SharePrice(uint256 indexed roundId, uint256 sharePrice);
 
     constructor(
         address _asset,
@@ -23,24 +30,35 @@ contract STETHBullVault is BaseVault {
         address _investor
     ) BaseVault(_asset, _strategist) {
         investor = _investor;
+        sharePriceDecimals = asset.decimals();
     }
 
     /**
      * @dev See {IVault-name}.
      */
     function name() external pure override returns (string memory) {
-        return "STETH Bull Vault";
+        return "stETH Vault";
     }
 
-    function _afterRoundStart(uint256 assets) internal override {
+    function _afterRoundStart(uint256) internal override {
         lastRoundAssets = totalAssets();
+        lastSharePrice = FixedPointMath.Fractional({
+            numerator: totalShares == 0 ? 0 : lastRoundAssets,
+            denominator: totalShares
+        });
+
+        uint256 sharePrice = lastSharePrice.denominator == 0 ? 0 : lastSharePrice.mulDivDown(10**sharePriceDecimals);
+        emit SharePrice(currentRoundId, sharePrice);
     }
 
     function _afterRoundEnd() internal override {
         uint256 roundAccruedInterest;
+        uint256 sharePrice;
         uint256 investmentYield = asset.balanceOf(investor);
+        uint256 idleAssets = asset.balanceOf(address(this));
 
         if (totalShares != 0) {
+            sharePrice = (totalAssets() + investmentYield).mulDivDown(10**sharePriceDecimals, totalShares);
             roundAccruedInterest = totalAssets() - lastRoundAssets;
 
             // Pulls the yields from investor
@@ -48,12 +66,19 @@ contract STETHBullVault is BaseVault {
                 asset.safeTransferFrom(investor, address(this), investmentYield);
             }
 
-            // Send round investment to investor
-            uint256 investment = (roundAccruedInterest * investorRatio) / DENOMINATOR;
-            if (investment > 0) {
-                asset.safeTransfer(investor, investment);
+            // Sends another batch to Investor
+            uint256 investmentAmount = (roundAccruedInterest * investorRatio) / DENOMINATOR;
+            if (investmentAmount > 0) {
+                asset.safeTransfer(investor, investmentAmount);
             }
         }
+
+        emit RoundData(currentRoundId, roundAccruedInterest, investmentYield, idleAssets);
+        emit SharePrice(currentRoundId, sharePrice);
+    }
+
+    function _beforeWithdraw(uint256 shares, uint256) internal override {
+        lastRoundAssets -= shares.mulDivDown(lastSharePrice);
     }
 
     /**
