96c9e4269dbbb628fb1e85f0e84803cbffcb2451
Robsonsjre <robsonsjre@gmail.com>
2022-06-22 12:13:08 -0300

    hotfix: fix linter

--- scripts/deployTestnet.ts
+++ scripts/deployTestnet.ts
@@ -17,9 +17,6 @@ async function main (): Promise<void> {
   await configurationManager.deployTransaction.wait(WAIT_CONFIRMATIONS)
   console.log(`\nConfigurationManager deployed at: ${configurationManager.address}\n`)
 
-  await configurationManager.setParameter(ethers.utils.formatBytes32String('VAULT_CONTROLLER'), deployer.address)
-  await configurationManager.setParameter(ethers.utils.formatBytes32String('WITHDRAW_FEE_RATIO'), BigNumber.from('100'))
-
   await verifyContract(hre, configurationManager.address, [])
 
   const assetName = 'Liquid staked Ether 2.0'
@@ -57,7 +54,6 @@ async function main (): Promise<void> {
   const vaultConstructorArguments = [
     configurationManager.address,
     asset.address,
-    deployer.address,
     investor.address,
     yieldSource.address
   ] as const
@@ -66,6 +62,9 @@ async function main (): Promise<void> {
   await vault.deployTransaction.wait(WAIT_CONFIRMATIONS)
   console.log(`\nVault deployed at: ${vault.address}\n`)
   await verifyContract(hre, vault.address, vaultConstructorArguments)
+
+  await configurationManager.setParameter(vault.address, ethers.utils.formatBytes32String('VAULT_CONTROLLER'), deployer.address)
+  await configurationManager.setParameter(vault.address, ethers.utils.formatBytes32String('WITHDRAW_FEE_RATIO'), BigNumber.from('100'))
 
   /* eslint-disable-next-line @typescript-eslint/no-floating-promises */
   await (await investor.approveVaultToPull(vault.address)).wait(WAIT_CONFIRMATIONS)
--- scripts/populateTestnet.ts
+++ scripts/populateTestnet.ts
@@ -5,10 +5,10 @@
 // Runtime Environment's members available in the global scope.
 import { ethers } from 'hardhat'
 
-const WAIT_CONFIRMATIONS = 5
+const WAIT_CONFIRMATIONS = 3
 
 async function main (): Promise<void> {
-  const vaultAddress = process.env.VAULT ?? '0x3B4bBe9c0A48AD148c5C54221c66DEE42668Cfef'
+  const vaultAddress = process.env.VAULT ?? '0x89b1D431a6114d4DeE6141A0188258122b8d2F41'
   if (!ethers.utils.isAddress(vaultAddress)) {
     throw new Error('Invalid vault address')
   }
@@ -16,15 +16,22 @@ async function main (): Promise<void> {
   const [,user0, user1] = await ethers.getSigners()
   const vault = await ethers.getContractAt('PrincipalProtectedMock', vaultAddress)
   const asset = await ethers.getContractAt('Asset', await vault.asset())
+  const yieldSource = await ethers.getContractAt('YieldSourceMock', await vault.yieldSource())
 
-  await (await asset.connect(user0).mint(ethers.utils.parseEther('250'))).wait(WAIT_CONFIRMATIONS)
+  await (await asset.connect(user0).mint(ethers.utils.parseEther('2'))).wait(WAIT_CONFIRMATIONS)
+  console.log('User0 Minted')
   await (await asset.connect(user0).approve(vault.address, ethers.constants.MaxUint256)).wait(WAIT_CONFIRMATIONS)
-  await (await vault.connect(user0).deposit(ethers.utils.parseEther('250'), user0.address)).wait(WAIT_CONFIRMATIONS)
+  console.log('User0 Approve')
+  await (await vault.connect(user0).deposit(ethers.utils.parseEther('2'), user0.address)).wait(WAIT_CONFIRMATIONS)
   console.log('User0 Deposited')
 
-  await (await asset.connect(user1).mint(ethers.utils.parseEther('100'))).wait(WAIT_CONFIRMATIONS)
+  await (await asset.connect(user1).mint(ethers.utils.parseEther('0.5'))).wait(WAIT_CONFIRMATIONS)
+  console.log('User1 Minted')
+
   await (await asset.connect(user1).approve(vault.address, ethers.constants.MaxUint256)).wait(WAIT_CONFIRMATIONS)
-  await (await vault.connect(user1).deposit(ethers.utils.parseEther('100'), user1.address)).wait(WAIT_CONFIRMATIONS)
+  console.log('User1 Approved')
+
+  await (await vault.connect(user1).deposit(ethers.utils.parseEther('0.5'), user1.address)).wait(WAIT_CONFIRMATIONS)
   console.log('User1 Deposited')
 
   await (await vault.endRound()).wait(WAIT_CONFIRMATIONS)
@@ -34,8 +41,16 @@ async function main (): Promise<void> {
   await (await vault.startRound()).wait(WAIT_CONFIRMATIONS)
   console.log('Round Started')
 
-  await (await vault.connect(user0).withdraw(user0.address)).wait(WAIT_CONFIRMATIONS)
-  console.log('User0 Withdrew')
+  await (await yieldSource.generateInterest(ethers.utils.parseEther('1'))).wait(WAIT_CONFIRMATIONS)
+
+  await (await vault.endRound()).wait(WAIT_CONFIRMATIONS)
+  console.log('Round Ended 2')
+
+  await (await vault.startRound()).wait(WAIT_CONFIRMATIONS)
+  console.log('Round Started 2')
+
+  await (await vault.connect(user1).redeem(await vault.balanceOf(user1.address), user1.address, user1.address)).wait(WAIT_CONFIRMATIONS)
+  console.log('User1 Withdrew')
 }
 
 // We recommend this pattern to be able to use async/await everywhere
