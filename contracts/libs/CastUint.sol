3aca3877be97bcb8deed4c198f9aa77cc2b71e5b
Guilherme Guimarães <ggviana@hotmail.com.br>
2022-09-20 04:43:03 -0300

    Simplifing Address Cast (#34)

--- contracts/libs/CastUint.sol
+++ contracts/libs/CastUint.sol
@@ -7,11 +7,7 @@ library CastUint {
      * @dev Converts a `uint256` to `address`
      */
     function toAddress(uint256 value) internal pure returns (address) {
-        bytes memory data = new bytes(32);
-        // solhint-disable-next-line no-inline-assembly
-        assembly {
-            mstore(add(data, 32), value)
-        }
-        return abi.decode(data, (address));
+        if (value == 0) return address(0);
+        return address(uint160(value));
     }
 }
