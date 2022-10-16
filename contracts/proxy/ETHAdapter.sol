b702189dcc11f552b440c96072e1838e0aff91a8
Guilherme Viana <ggviana@hotmail.com.br>
2022-10-16 20:44:42 -0300

    [N-12] Removed unused imports

--- contracts/proxy/ETHAdapter.sol
+++ contracts/proxy/ETHAdapter.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
-import "../interfaces/IConfigurationManager.sol";
 import "../interfaces/ICurvePool.sol";
 import "../interfaces/IVault.sol";
 
