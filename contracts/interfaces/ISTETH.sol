f91306e4fb55983cea1e4da3acb1d3a7449f91b9
Robsonsjre <robsonsjre@gmail.com>
2022-11-23 22:52:23 -0300

    feat: add stETH interface file

---
+++ contracts/interfaces/ISTETH.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+pragma solidity 0.8.17;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface STETH is IERC20 {
+    function getTotalPooledEther() external view returns (uint256);
+
+    function decimals() external view returns (uint8);
+
+    function symbol() external view returns (string memory);
+}
