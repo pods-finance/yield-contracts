4f1d84b7ecce30541ce4bef1c4aa96b0b376e591
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-03-06 14:48:36 -0300

    Fix invariant tests
    - Change state variables from public to private
    - Invariant test do not extend from STETHVault
    - Change STETHVault to STETHVaultHarness

--- test/invariants/STETHVaultInvariants.sol
+++ test/invariants/STETHVaultInvariants.sol
@@ -34,20 +34,38 @@ library String {
 }
 
 contract FuzzyAddresses {
-    address public constant user0 = address(0x10000);
-    address public constant user1 = address(0x20000);
-    address public constant vaultController = address(0x30000);
+    address internal constant user0 = address(0x10000);
+    address internal constant user1 = address(0x20000);
+    address internal constant vaultController = address(0x30000);
 
     function _addressIsAllowed(address to) internal returns (bool) {
         return to == user0 || to == user1 || to == vaultController;
     }
 }
 
-contract STETHVaultInvariants is STETHVault, FuzzyAddresses {
-    ConfigurationManager public $configuration = new ConfigurationManager();
-    STETH public $asset = new STETH();
-    InvestorActorMock public $investor = new InvestorActorMock(address($asset));
-    event AssertionFailed(bool);
+contract STETHVaultHarness is STETHVault {
+    constructor(
+        IConfigurationManager _configuration,
+        IERC20Metadata _asset,
+        address _investor
+    ) STETHVault(_configuration, _asset, _investor) {}
+
+    // HARNESS: changed to public from internal
+    function ___vaultState() public returns (VaultState memory) {
+        return vaultState;
+    }
+
+    // HARNESS: changed to public from internal
+    function ___convertToShares(uint256 assets, Math.Rounding rounding) public view returns (uint256 shares) {
+        return _convertToShares(assets, rounding);
+    }
+}
+
+contract STETHVaultInvariants is FuzzyAddresses {
+    ConfigurationManager private $configuration = new ConfigurationManager();
+    STETH private $asset = new STETH();
+    InvestorActorMock private $investor = new InvestorActorMock(address($asset));
+    STETHVaultHarness private vault = new STETHVaultHarness($configuration, $asset, address($investor));
 
     struct LastDeposit {
         uint256 amount;
@@ -55,43 +73,43 @@ contract STETHVaultInvariants is STETHVault, FuzzyAddresses {
         uint256 shares;
     }
 
-    mapping(address => LastDeposit) public lastDeposits;
+    mapping(address => LastDeposit) private lastDeposits;
 
-    constructor() STETHVault($configuration, $asset, address($investor)) {
-        $configuration.setParameter(address(this), "VAULT_CONTROLLER", 0x30000);
+    constructor() {
+        $configuration.setParameter(address(this), "VAULT_CONTROLLER", uint256(uint160(vaultController)));
     }
 
     function echidna_test_name() public view returns (bool) {
-        return String.equal(name(), "stETH Volatility Vault");
+        return String.equal(vault.name(), "stETH Volatility Vault");
     }
 
     function echidna_test_symbol() public returns (bool) {
-        return String.equal(symbol(), "stETHvv");
+        return String.equal(vault.symbol(), "stETHvv");
     }
 
     function echidna_test_decimals() public returns (bool) {
-        return decimals() == $asset.decimals();
+        return vault.decimals() == $asset.decimals();
     }
 
     function generateInterest(uint256 a) public {
         if (a == 0) return;
-        uint256 addInterest = (totalAssets() / a);
+        uint256 addInterest = (vault.totalAssets() / a);
         $asset.generateInterest(addInterest);
     }
 
     //  ["0x10000", "0x20000", "0x30000"]
     function echidna_sum_total_supply() public returns (bool) {
-        uint256 balanceA = balanceOf(user0);
-        uint256 balanceB = balanceOf(user1);
-        uint256 balanceC = balanceOf(vaultController);
+        uint256 balanceA = vault.balanceOf(user0);
+        uint256 balanceB = vault.balanceOf(user1);
+        uint256 balanceC = vault.balanceOf(vaultController);
 
         uint256 sumBalances = balanceA + balanceB + balanceC;
 
-        return sumBalances == totalSupply();
+        return sumBalances == vault.totalSupply();
     }
 
     function echidna_lastRoundAssets_always_greater_than_totalAssets() public returns (bool) {
-        return totalAssets() >= lastRoundAssets;
+        return vault.totalAssets() >= vault.lastRoundAssets();
     }
 
     /**
@@ -101,41 +119,41 @@ contract STETHVaultInvariants is STETHVault, FuzzyAddresses {
      */
     //
     function helpProcessQueue() public {
-        this.processQueuedDeposits(this.queuedDeposits());
+        vault.processQueuedDeposits(vault.queuedDeposits());
     }
 
-    function deposit(uint256 assets, address) public override returns (uint256 shares) {
-        uint256 createdShares = convertToShares(assets);
+    function deposit(uint256 assets, address) public returns (uint256 shares) {
+        uint256 createdShares = vault.convertToShares(assets);
         LastDeposit memory newDeposit = LastDeposit({
             amount: assets,
-            roundId: vaultState.currentRoundId,
+            roundId: vault.___vaultState().currentRoundId,
             shares: createdShares
         });
         lastDeposits[msg.sender] = newDeposit;
-        return this.deposit(assets, msg.sender);
+        return vault.deposit(assets, msg.sender);
     }
 
-    function mint(uint256 shares, address) public override returns (uint256 assets) {
-        uint256 assets2 = convertToAssets(shares);
+    function mint(uint256 shares, address) public returns (uint256 assets) {
+        uint256 assets2 = vault.convertToAssets(shares);
         LastDeposit memory newDeposit = LastDeposit({
             amount: assets2,
-            roundId: vaultState.currentRoundId,
+            roundId: vault.___vaultState().currentRoundId,
             shares: shares
         });
         lastDeposits[msg.sender] = newDeposit;
-        return this.mint(shares, msg.sender);
+        return vault.mint(shares, msg.sender);
     }
 
     function withdraw(
         uint256 assets,
         address,
         address
-    ) public override returns (uint256 shares) {
-        bool isNextRound = vaultState.currentRoundId == lastDeposits[msg.sender].roundId + 1;
-        uint256 burnShares = _convertToShares(assets, Math.Rounding.Up);
+    ) public returns (uint256 shares) {
+        bool isNextRound = vault.___vaultState().currentRoundId == lastDeposits[msg.sender].roundId + 1;
+        uint256 burnShares = vault.___convertToShares(assets, Math.Rounding.Up);
 
-        this.withdraw(assets, msg.sender, msg.sender);
-        uint256 userSharesAfterWithdraw = this.balanceOf(msg.sender);
+        vault.withdraw(assets, msg.sender, msg.sender);
+        uint256 userSharesAfterWithdraw = vault.balanceOf(msg.sender);
 
         if (isNextRound && assets > 0) {
             if (burnShares < lastDeposits[msg.sender].shares) {
@@ -143,10 +161,7 @@ contract STETHVaultInvariants is STETHVault, FuzzyAddresses {
                 lastDeposits[msg.sender].amount -= assets;
             }
             if (burnShares == lastDeposits[msg.sender].shares || userSharesAfterWithdraw == 0) {
-                bool isWithdrawLowerThanInitial = assets < lastDeposits[msg.sender].amount;
-                if (isWithdrawLowerThanInitial) {
-                    emit AssertionFailed(isWithdrawLowerThanInitial);
-                }
+                assert(assets >= lastDeposits[msg.sender].amount);
             }
         }
     }
@@ -155,36 +170,33 @@ contract STETHVaultInvariants is STETHVault, FuzzyAddresses {
         uint256 shares,
         address,
         address
-    ) public override returns (uint256 assets) {
-        bool isNextRound = vaultState.currentRoundId == lastDeposits[msg.sender].roundId + 1;
-        uint256 assetsToWithdraw = convertToAssets(shares);
-        uint256 balanceOfShares = this.balanceOf(msg.sender);
-        this.redeem(shares, msg.sender, msg.sender);
+    ) public returns (uint256 assets) {
+        bool isNextRound = vault.___vaultState().currentRoundId == lastDeposits[msg.sender].roundId + 1;
+        uint256 assetsToWithdraw = vault.convertToAssets(shares);
+        uint256 balanceOfShares = vault.balanceOf(msg.sender);
+        vault.redeem(shares, msg.sender, msg.sender);
         if (isNextRound && shares > 0) {
             if (shares < lastDeposits[msg.sender].shares) {
                 lastDeposits[msg.sender].shares -= shares;
                 lastDeposits[msg.sender].amount -= assetsToWithdraw;
             }
             if (shares == lastDeposits[msg.sender].shares || shares == balanceOfShares) {
-                bool isWithdrawLowerThanInitial = assetsToWithdraw < lastDeposits[msg.sender].amount;
-                if (isWithdrawLowerThanInitial) {
-                    emit AssertionFailed(isWithdrawLowerThanInitial);
-                }
+                assert(lastDeposits[msg.sender].amount >= assetsToWithdraw);
             }
         }
     }
 
-    function transfer(address to, uint256 amount) public override(ERC20, IERC20) returns (bool) {
+    function transfer(address to, uint256 amount) public returns (bool) {
         if (!_addressIsAllowed(to)) return false;
-        return super.transfer(to, amount);
+        return vault.transfer(to, amount);
     }
 
     function transferFrom(
         address from,
         address to,
         uint256 amount
-    ) public override(ERC20, IERC20) returns (bool) {
+    ) public returns (bool) {
         if (!_addressIsAllowed(to)) return false;
-        return super.transferFrom(from, to, amount);
+        return vault.transferFrom(from, to, amount);
     }
 }
