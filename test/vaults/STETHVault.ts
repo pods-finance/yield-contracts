db38711dc4bc1d283c0ced61b38c30403c47f39a
Guilherme Guimarães <ggviana@hotmail.com.br>
2023-03-10 13:14:28 -0300

    Fixed tests

--- test/vaults/STETHVault.ts
+++ test/vaults/STETHVault.ts
@@ -71,11 +71,15 @@ describe('STETHVault', () => {
     const InvestorActorMock = await ethers.getContractFactory('InvestorActorMock')
     investor = await InvestorActorMock.deploy(asset.address)
 
+    // The precision of this number is set by the variable DENOMINATOR. 5000 is equivalent to 50%
+    const investorRatio = ethers.BigNumber.from('5000')
+
     const STETHVault = await ethers.getContractFactory('STETHVault')
     vault = await STETHVault.deploy(
       configuration.address,
       asset.address,
-      investor.address
+      investor.address,
+      investorRatio
     )
 
     // Give approval upfront that the vault can pull money from the investor contract
@@ -980,7 +984,8 @@ describe('STETHVault', () => {
     const vault = await STETHVault.deploy(
       configuration.address,
       asset.address,
-      investor.address
+      investor.address,
+      5000
     )
 
     await investor.approveVaultToPull(vault.address)
